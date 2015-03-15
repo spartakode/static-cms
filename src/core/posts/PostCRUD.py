5824529fa763956e353acd1cf3e23984e6b5ddc5
Adnan Issadeen <spartakode@outlook.com>
2015-03-15 19:44:29 +0530

    Added basic save and retrieval methods for posts. Addresses #5

---
+++ src/core/posts/PostCRUD.py
@@ -0,0 +1,5 @@
+def savePost(postToSave, postDataStrategy):
+    return postDataStrategy.savePost(postToSave)
+
+def getMainPagePosts(postCount, postDataStrategy):
+    return postDataStrategy.getMainPagePosts(postCount)
--- src/core/tests/TestPost.py
+++ src/core/tests/TestPost.py
@@ -1,7 +1,9 @@
 import unittest
+from unittest.mock import MagicMock, Mock
 from datetime import date
 
 from ..posts.Post import Post
+from ..posts import PostCRUD
 
 class TestPostObject(unittest.TestCase):
     def testPostObjectCreatedCorrectly(self):
@@ -13,6 +15,26 @@ class TestPostObject(unittest.TestCase):
         self.assertEqual(postObjecToTest.postUrl, "a-sample-post")
         self.assertEqual(postObjecToTest.postLink, "")
         self.assertEqual(postObjecToTest.isLinkBlog, False)
+
+class TestPostCRUD(unittest.TestCase):
+    def setUp(self):
+        self.postObjecToTest = Post("A sample post", """<p>The post body</p>\n[image 1 center]\n<p><p>The ending paragraph</p>""",
+                date(2015,3,1), "a-sample-post")
+        self.postObjects = []
+        for i in range(0,10):
+            self.postObjects.append(Post("A sample post", """<p>The post body</p>\n[image 1 center]\n<p><p>The ending paragraph</p>""",
+                date(2015,3,1), "a-sample-post%d"%i))
+        self.mockProductDataStrategy = Mock()
+        mockProductDataStrategyAttrs = {"savePost.return_value": True,
+                "getMainPagePosts.return_value": self.postObjects}
+        self.mockProductDataStrategy.configure_mock(**mockProductDataStrategyAttrs)
+
+    def testPostSavesCorrectly(self):
+        self.assertTrue(PostCRUD.savePost(self.postObjecToTest, self.mockProductDataStrategy))
+
+    def testPostsRetrieveCorrectly(self):
+        self.assertEqual(PostCRUD.getMainPagePosts(20, self.mockProductDataStrategy), self.postObjects)
+
 
 if __name__ == "__main__":
     unittest.main()
