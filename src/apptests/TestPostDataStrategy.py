7c7c39745fe4d89d37b9968d0eda2c1d247504e7
Adnan Issadeen <spartakode@outlook.com>
2015-03-16 21:51:42 +0530

    Added sqlite Post Data Strategy

---
+++ src/apptests/TestPostDataStrategy.py
@@ -0,0 +1,36 @@
+import unittest
+import sqlite3
+from datetime import date
+
+from ..core.posts.Post import Post
+from ..core.posts import PostCRUD
+from ..data.sqlite import PostDataStrategy
+
+class TestPosDataStrategy(unittest.TestCase):
+    def setUp(self):
+        self.postObjecToTest = Post("A sample post", """<p>The post body</p>\n[image 1 center]\n<p><p>The ending paragraph</p>""",
+                date(2015,3,1), "a-sample-post")
+        conn = sqlite3.connect("src/data/sqlite/staticcms.db")
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("DELETE FROM posts")
+        conn.commit()
+        conn.close()
+    def tearDown(self):
+        conn = sqlite3.connect("src/data/sqlite/staticcms.db")
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("DELETE FROM posts")
+        conn.commit()
+        conn.close()
+
+    def testPostSavesCorrectly(self):
+        self.assertTrue(PostCRUD.savePost(self.postObjecToTest, PostDataStrategy))
+
+    def testMainPagePostsRetrieveCorrectly(self):
+        PostCRUD.savePost(self.postObjecToTest, PostDataStrategy)
+        posts = PostCRUD.getMainPagePosts(10, PostDataStrategy)
+        self.assertEqual(len(posts), 1)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/data/sqlite/PostDataStrategy.py
@@ -0,0 +1,38 @@
+import sqlite3
+from datetime import date
+
+from ...core.posts.Post import Post
+def savePost(postToSave):
+    conn = sqlite3.connect("src/data/sqlite/staticcms.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("""INSERT INTO posts (postTitle,
+            postBody,
+            postDate,
+            postUrl,
+            postLink) VALUES (?,?,?,?,?)""",
+            (postToSave.postTitle,
+                postToSave.postBody,
+                postToSave.postDate,
+                postToSave.postUrl,
+                postToSave.postLink))
+    conn.commit()
+    conn.close()
+    return True
+
+def getMainPagePosts(postCount):
+    conn = sqlite3.connect("src/data/sqlite/staticcms.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM posts LIMIT %d"%(postCount))
+    results = cur.fetchall()
+    postsToReturn = []
+    for result in results:
+        dateParts = result['postDate'].split('-')
+        postDate = (date(int(dateParts[0]), int(dateParts[1]), int(dateParts[2])))
+        postsToReturn.append(Post(result['postTitle'],
+            result['postBody'],
+            postDate,
+            result['postUrl'],
+            result['postLink']))
+    return postsToReturn
