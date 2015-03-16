9a5affbab4e29d6bdd61196d2df02af83309b215
Adnan Issadeen <spartakode@outlook.com>
2015-03-17 00:41:48 +0530

    Stylesheet is added to blog directory if it doesnt exist

--- serverstart.py
+++ serverstart.py
@@ -1,7 +1,15 @@
 from src import application
 import argparse
+import os
+import configparser
+import shutil
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('src/config.ini')
+    siteLocation = config['SITEADMIN']['fileLocation']
+    if not os.path.exists(siteLocation+'/static/stylesheets/style.css'):
+        shutil.copytree('src/core/tests/testdata/static/', siteLocation+'/static')
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
@@ -9,4 +17,3 @@ if __name__ == "__main__":
         application.run(host='0.0.0.0', port=5000, debug=True)
     else:
         application.run(host='0.0.0.0', port=5000, debug=False)
-
