aad788db9bde24740b4e7fc9477624ecbe5d7de4
Adnan Issadeen <spartakode@outlook.com>
2015-05-27 23:45:51 +0530

    Basic RSS generation now enabled. Addresses #18
    Need to finetune it and write tests. This was more a proof of concept or
    research that somehow ended up actually completing a lot of the feature.

--- src/core/statichtml/HtmlGenerator.py
+++ src/core/statichtml/HtmlGenerator.py
@@ -1,6 +1,8 @@
 import configparser
 import os, shutil
+from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
+import PyRSS2Gen as RSS2
 
 from ..posts import PostRetrieval
 def generateHtmlForPostPage(post):
@@ -35,13 +37,51 @@ def generateHtmlForMainPage(postDataStrategy):
     mainPagePosts = PostRetrieval.getMainPagePosts(int(mainPageConfigurations['numberOfPosts']), postDataStrategy)
     for post in mainPagePosts:
         post.renderedPostBody = post.postBody
+    generateRss(mainPagePosts)
     return mainPageTemplate.render(title=title,
             posts = mainPagePosts,
             feedlyButtonInformation = postConfigurations['feedlyButtonInformation'],
             googleAnalyticsKey = postConfigurations['googleAnalyticsKey'],
             googleAnalyticsDomain = postConfigurations['googleAnalyticsDomain'],
             disqusShortName = postConfigurations['disqusShortName'])
+
+def generateRss(posts):
+    pageTitle = "Adnan Issadeen Meets Blogging"
+    pageLink = "http://adnanissadeen.com"
+    pageDescription = "The blog from the world of adnan issadeen"
+    pageLastBuildDate= datetime.now()
     
+    items = []
+    for post in posts:
+        title = post.postTitle
+        if post.postLink:
+            link = post.postLink + " &#8594;"
+            permaLink = False
+        else:
+            link = "http://adnanissadeen.com/%s.html"%(post.postUrl)
+            permaLink = True
+        items.append(RSS2.RSSItem(
+            title = title,
+            link = link,
+            description = post.postBody,
+            guid = RSS2.Guid(link, permaLink),
+            pubDate = datetime(post.postDate.year,
+                post.postDate.month,
+                post.postDate.day),
+            )
+        )
+    rss = RSS2.RSS2(
+                title=pageTitle,
+                link = pageLink,
+                description = pageDescription,
+                lastBuildDate = pageLastBuildDate,
+
+                items = items
+            )
+    configurations = getConfigurations()
+    siteDirectory = configurations['SITEADMIN']['fileLocation']
+    rss.write_xml(open(os.path.join(siteDirectory, "pyrss2gen.xml"), "w"))
+
 def getConfigurations():
     config = configparser.ConfigParser()
     config.read("src/config.ini")
